f6d93f303d57c9cd0e4196e1c36dc95761d6bb91
Deepak <chaubeydeepak903@gmail.com>
2024-02-14 17:39:28 +0530

    updating changes

--- app.js
+++ app.js
@@ -1,14 +1,73 @@
 const express = require('express');
+const path = require("path");
 
 const app = express()
-const PORT = 8000
+const PORT = 8000;
+const nodeMailer = require('nodemailer');
+const Mailgen = require('mailgen');
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-app.get('/about', (req, res) => {
-  res.send('About route 🎉 ')
+app.post("/send", async (req, res) => {
+    try {
+        const name = req.body.name;
+        const email = req.body.email;
+        const subj = req.body.subject;
+        const query = req.body.query;
+
+        const mailsend = async () => {
+            let config = {
+                service: 'gmail',
+                auth: {
+                    user: process.env.EMAIL,
+                    pass: process.env.PASSWORD
+                }
+            };
+
+            let transporter = nodeMailer.createTransport(config);
+
+            let MailGenerator = new Mailgen({
+                theme: "default",
+                product: {
+                    name: "Mailgen",
+                    link: 'https://mailgen.js'
+                }
+            });
+
+            let message = {
+                from: process.env.EMAIL,
+                to: process.env.RECEIVER,
+                subject: subj,
+                html: `<div class="contaier">
+                <p>Name: ${name}</p>
+                <p>from: ${email}</p>
+                <p>query:</p>
+                <p>${query}</p>
+            </div>`
+            };
+
+            try {
+                 const mailsended = await transporter.sendMail(message);
+                res.status(201).sendFile(path.join(__dirname, 'index.html'));
+            }
+            catch (err) {
+                console.log(err);
+                res.status(500).send(err);
+            }
+        };
+        await mailsend();
+    } catch (err) {
+        res.status(400).send(err);
+    }
+
+
 })
 
 app.listen(PORT, () => {
